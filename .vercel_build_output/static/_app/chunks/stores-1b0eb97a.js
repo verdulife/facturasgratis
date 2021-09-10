037b9f35c397bdf991733cd27b8fe953ea353097
verdulife <verdu@live.com>
2021-09-10 14:13:29 +0200

    init cropper

--- .vercel_build_output/static/_app/chunks/stores-1b0eb97a.js
+++
@@ -1,1 +0,0 @@
-import{C as a}from"./vendor-dc9a6933.js";const r=a(JSON.parse(localStorage.getItem("userData"))||{});r.subscribe((a=>localStorage.userData=JSON.stringify(a)));export{r as u};
--- src/routes/ajustes/index.svelte
+++ src/routes/ajustes/index.svelte
@@ -1,5 +1,38 @@
 <script>
   import { userData } from "../../stores";
+  import Cropper from "svelte-easy-crop";
+  import { getCroppedImg } from "../../lib/canvas-utils";
+
+  let image, fileinput, pixelCrop, croppedImage;
+
+  function onFileSelected(e) {
+    let imageFile = e.target.files[0];
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      image = e.target.result;
+    };
+    reader.readAsDataURL(imageFile);
+  }
+
+  let profilePicture, style;
+
+  function previewCrop(e) {
+    pixelCrop = e.detail.pixels;
+    const { x, y, width } = e.detail.pixels;
+    const scale = 200 / width;
+    profilePicture.style = `margin: ${-y * scale}px 0 0 ${-x * scale}px; width: ${
+      profilePicture.naturalWidth * scale
+    }px;`;
+  }
+
+  async function cropImage() {
+    croppedImage = await getCroppedImg(image, pixelCrop);
+  }
+
+  function reset() {
+    croppedImage = null;
+    image = null;
+  }
 </script>
 
 <svelte:head>
@@ -17,12 +50,45 @@
 
   {#if $userData}
     <div class="info col acenter xfill">
-      <div class="box round col xfill">
+      <!-- <div class="box round col xfill">
         <h2>Logotipo</h2>
         <p class="notice">Si usas logotipo en tus facturas, presupuestos o albaranes, aqui es el sitio.</p>
 
         <input type="file" />
-      </div>
+      </div> -->
+
+      {#if !image}
+        <h2>Upload a picture for cropping?</h2>
+        <input type="file" accept=".jpg, .jpeg, .png" on:change={(e) => onFileSelected(e)} bind:this={fileinput} />
+        <h2>Or... use this cute dog 🐕</h2>
+        <button
+          type="button"
+          on:click={() => {
+            image = defaultSrc;
+          }}>Click me!</button
+        >
+      {:else}
+        <h2>svelte-easy-crop</h2>
+        <div style="position: relative; width: 100%; height: 50%;">
+          <Cropper {image} aspect={1} zoom="1" crop={{ x: 0, y: 0 }} on:cropcomplete={previewCrop} />
+        </div>
+        <h2>Preview</h2>
+        <div class="prof-pic-wrapper">
+          <img bind:this={profilePicture} class="prof-pic" src={image} alt="Profile example" {style} />
+        </div>
+        {#if croppedImage}
+          <h2>Cropped Output</h2>
+          <img src={croppedImage} alt="Cropped profile" /><br />
+        {:else}
+          <br /><button
+            type="button"
+            on:click={async () => {
+              croppedImage = await getCroppedImg(image, pixelCrop);
+            }}>Crop!</button
+          >
+        {/if}
+        <button type="button" on:click={reset}>Start over?</button>
+      {/if}
 
       <div class="box round col xfill">
         <h2>Datos legales</h2>
@@ -257,5 +323,17 @@
         font-size: 14px;
       }
     }
+  }
+
+  .prof-pic-wrapper {
+    height: 200px;
+    width: 200px;
+    position: relative;
+    border: solid;
+    overflow: hidden;
+  }
+
+  .prof-pic {
+    position: absolute;
   }
 </style>
