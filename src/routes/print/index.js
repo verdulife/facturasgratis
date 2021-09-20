6d6874ccbaa3dabd5d983c85c52925bae69c5afa
verdulife <verdu@live.com>
2021-09-20 14:45:56 +0200

    netlify

--- src/routes/print/index.js
+++ src/routes/print/index.js
@@ -55,7 +55,7 @@ export async function post(req) {
 
     body: doc.output("datauristring"),
   };
-}
+} */
 
 import PDFDocument from "pdfkit-next";
 import SVGtoPDF from "svg-to-pdfkit";
@@ -89,10 +89,12 @@ export async function post(req) {
 
   doc.text(data.number, mm(168), mm(31));
   doc.text(`${data.date.day}/${data.date.month}/${data.date.year}`, mm(168), mm(36));
+
   doc.text(data.client.legal_name, mm(34.5), mm(66.7));
   doc.text(data.client.legal_id, mm(163.5), mm(66.7));
   doc.text(data.client.address, mm(42), mm(74));
   doc.text(data.client.contact, mm(163.5), mm(74));
+
   for (let l = 0; l < data.items.length; l++) {
     const line = data.items[l];
     const jump = mm(5.5) * l;
@@ -104,6 +106,7 @@ export async function post(req) {
     doc.text(`${line.price.toFixed(2)}€`, mm(150), mm(97) + jump);
     doc.text(`${line_total.toFixed(2)}€`, mm(168), mm(97) + jump);
   }
+
   doc.text(`${data.totals.base.toFixed(2)}€`, mm(63.5), mm(238));
   if (data.totals.ret > 0) doc.text(`-${data.totals.ret.toFixed(2)}€`, mm(96), mm(238));
   doc.text(`${data.totals.iva.toFixed(2)}€`, mm(128), mm(238));
@@ -127,4 +130,3 @@ export async function post(req) {
     body: await pdfBuffer,
   };
 }
- */
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,22 @@
-import vercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
-import autoprefixer from 'autoprefixer'
+import vercel from "@sveltejs/adapter-vercel";
+import netlify from "@sveltejs/adapter-netlify";
+import preprocess from "svelte-preprocess";
+import autoprefixer from "autoprefixer";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		scss: {
-			prependData: '@import "./src/_vars.scss";'
-		},
-		postcss: {
-			plugins: [autoprefixer]
-		}
-	}),
-	kit: {
-		target: '#svelte',
-		adapter: vercel()
-	}
+  preprocess: preprocess({
+    scss: {
+      prependData: '@import "./src/_vars.scss";',
+    },
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  }),
+  kit: {
+    adapter: netlify(),
+    target: "#svelte",
+  },
 };
 
 export default config;
