625887a3644fcaaa6b87353d9f8d8c2a2a638571
verdulife <verdu@live.com>
2021-10-11 18:25:27 +0200

    calc

--- src/routes/calculadora/index.svelte
+++ src/routes/calculadora/index.svelte
@@ -1,10 +1,32 @@
 <script>
-  import { calculadora } from "../../lib/metadata";
   import { userData } from "../../lib/stores";
+  import { tools } from "../../lib/utils";
+  import { calculadora } from "../../lib/metadata";
+  import { roundWithTwoDecimals } from "../../lib/functions";
   import Comingsoon from "../../components/Comingsoon.svelte";
 
-  const IVA = $userData.iva || 21;
-  const IRPF = $userData.ret || 0;
+  let IVA = $userData.iva || 21;
+  let IRPF = $userData.ret || 0;
+  let user_value = 0;
+  let with_iva = 0;
+  let with_irpf = 0;
+  let total = 0;
+
+  function clearCalc() {
+    user_value = 0;
+  }
+
+  function deleteLastCharacter() {
+    if (user_value === 0 || user_value === "0" || !user_value) user_value = 0;
+    else user_value = user_value.slice(0, -1);
+  }
+
+  function add(c) {
+    if (user_value === 0 || user_value === "0" || !user_value) user_value = c;
+    else user_value += c;
+  }
+
+  function calc() {}
 </script>
 
 <svelte:head>
@@ -32,29 +54,86 @@
 </svelte:head>
 
 <Comingsoon />
-<!-- <div class="col fill">
+
+<!-- <div class="scroll">
   <article class="header col fcenter xfill">
-    <h1>Calculadora de impuestos</h1>
-    <p>Una calculadora diseñada para calcular de forma automática los impuestos.</p>
+    <img src="/presupuestos.svg" alt="Calculadora de impuestos" />
+    <h1>{tools[7].title}</h1>
+    <p>{tools[7].desc}</p>
+
+    <h-div />
+
+    <small>Toca un valor para copiarlo</small>
+
+    <div class="output row xfill">
+      <div class="box jbetween xhalf">
+        <p>IVA</p>
+        <p>
+          <b>{roundWithTwoDecimals(with_iva).toFixed(2)}{$userData && $userData.currency ? $userData.currency : "€"}</b>
+        </p>
+      </div>
+
+      <div class="box jbetween xhalf">
+        <p>IRPF</p>
+        <p>
+          <b>{roundWithTwoDecimals(with_irpf).toFixed(2)}{$userData && $userData.currency ? $userData.currency : "€"}</b
+          >
+        </p>
+      </div>
+
+      <div class="box jbetween xfill">
+        <p>TOTAL</p>
+        <p>
+          <b>{roundWithTwoDecimals(total).toFixed(2)}{$userData && $userData.currency ? $userData.currency : "€"}</b>
+        </p>
+      </div>
+    </div>
   </article>
 
-  <div class="display box col aend">
-    <span class="col xfill">
-      <small class="label xfill nowrap">IVA</small>
-      <p class="label xfill nowrap">0.00€</p>
-    </span>
+  <section class="col xfill">
+    <div class="row xfill">
+      <div class="input-wrapper row acenter xhalf">
+        <label class="row fcenter yfill" for="iva_value">IVA %</label>
+        <input class="out yfill" id="iva_value" type="number" step="0.01" bind:value={IVA} placeholder="Ej. 21" />
+      </div>
 
-    {#if IRPF}
-      <span class="col xfill">
-        <small class="label xfill nowrap">IRPF</small>
-        <p class="label xfill nowrap">0.00€</p>
-      </span>
-    {/if}
-    <span class="col xfill">
-      <small class="label xfill nowrap">TOTAL</small>
-      <p class="label xfill nowrap">0.00€</p>
-    </span>
-  </div>
+      <div class="input-wrapper row acenter xhalf">
+        <label class="row fcenter yfill" for="irpf_value">IRPF %</label>
+        <input class="out yfill" id="irpf_value" type="number" step="0.01" bind:value={IRPF} placeholder="Ej. 15" />
+      </div>
+    </div>
+
+    <div class="display box row jend acenter xfill">
+      <span class="row fcenter yfill">BASE {$userData && $userData.currency ? $userData.currency : "€"}</span>
+      <p class="grow">{user_value}</p>
+    </div>
+
+    <div class="numpad row xfill">
+      <div class="box" on:click={clearCalc}>C</div>
+      <div class="box" on:click={deleteLastCharacter}>DEL</div>
+      <div class="box" on:click={() => add("/")}>/</div>
+      <div class="box" on:click={() => add("*")}>*</div>
+
+      <div class="box" on:click={() => add("7")}>7</div>
+      <div class="box" on:click={() => add("8")}>8</div>
+      <div class="box" on:click={() => add("9")}>9</div>
+      <div class="box" on:click={() => add("-")}>-</div>
+
+      <div class="box" on:click={() => add("4")}>4</div>
+      <div class="box" on:click={() => add("5")}>5</div>
+      <div class="box" on:click={() => add("6")}>6</div>
+      <div class="box" on:click={() => add("+")}>+</div>
+
+      <div class="box" on:click={() => add("1")}>1</div>
+      <div class="box" on:click={() => add("2")}>2</div>
+      <div class="box" on:click={() => add("3")}>3</div>
+      <div class="box vdbl" on:click={calc}>&nbsp;</div>
+
+      <div class="box dbl" on:click={() => add("0")}>0</div>
+      <div class="box" on:click={() => add(".")}>.</div>
+      <div class="box vdbl" on:click={calc}>=</div>
+    </div>
+  </section>
 </div> -->
 
 <style lang="scss">
@@ -62,21 +141,25 @@
     background: linear-gradient(45deg, $pri 50%, $sec);
     text-align: center;
     color: $white;
-    padding: 60px 40px;
+    padding: 40px;
 
     @media (max-width: $mobile) {
       padding: 20px;
+    }
+
+    img {
+      width: 100px;
+      margin-bottom: 20px;
     }
 
     h1 {
       max-width: 900px;
       font-size: 3vw;
       line-height: 1.2;
-      margin-bottom: 40px;
+      margin-bottom: 10px;
 
       @media (max-width: $mobile) {
         font-size: 5vh;
-        margin-bottom: 20px;
       }
     }
 
@@ -87,6 +170,105 @@
 
       @media (max-width: $mobile) {
         font-size: 14px;
+      }
+    }
+
+    h-div {
+      max-width: 900px;
+      margin: 30px 0;
+    }
+
+    small {
+      margin-bottom: 5px;
+      font-weight: lighter;
+      color: $sec;
+    }
+
+    .output {
+      max-width: 700px;
+
+      .box {
+        cursor: pointer;
+        background: rgba($sec, 0.1);
+        margin-bottom: -1px;
+        padding: 10px;
+
+        @media (max-width: $mobile) {
+          padding: 7px;
+        }
+
+        p {
+          text-align: left;
+          font-size: 10px;
+
+          b {
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+
+  section {
+    max-width: 700px;
+    margin: 0 auto;
+    padding: 20px;
+
+    @media (max-width: $mobile) {
+      padding: 20px 10px;
+    }
+
+    .input-wrapper {
+      height: 50px;
+
+      label {
+        width: 60px;
+        font-size: 10px;
+        border: 1px solid $border;
+      }
+
+      input {
+        width: calc(100% - 60px);
+      }
+    }
+  }
+
+  .display {
+    text-align: right;
+
+    span {
+      font-size: 10px;
+      padding-right: 10px;
+    }
+  }
+
+  .numpad {
+    .box {
+      cursor: pointer;
+      width: 25%;
+      text-align: center;
+      transition: 100ms;
+
+      &:active {
+        background: $sec;
+      }
+    }
+
+    .dbl {
+      width: 50%;
+    }
+
+    .vdbl {
+      background-color: $pri;
+      color: $white;
+      border-bottom-color: $pri;
+
+      &:last-of-type {
+        border-top-color: $pri;
+      }
+
+      &:active {
+        background: $pri;
       }
     }
   }
